f5689beede76b838ae5407dcc11e1ac1b5280f29
SylvieC <sylviecottrell@gmail.com>
2014-08-27 21:53:46 -0700

    added audio files

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -1,20 +1,9 @@
 
 $(document).ready(function(){
   var team = [];
-
-   $('#1').hide();
-   $('#show').hide();
-   $('#six').hide();
    $('#activatePlayer').hide();
    $('#restart').hide();
 
-  
-  $(".fivePlayers").click(function(){
-    $("#show").show();
-  });
-  $('#morgana').click(function(){
-   $('#1').show();
-  })
   var decks = [$(".box5"), $(".box6"), $(".box7"),$(".box8"), $(".box9"), $(".box10")];
   for (var i = 0 ; i < decks.length; i++){
      decks[i].hide();
@@ -22,46 +11,32 @@ $(document).ready(function(){
 
   $('#basic').hide();
   $('#basic').siblings().hide();
-  
-// $('#box6').click(function(){
-//   $('#six').show();
-//   $('.anim').addClass('animated tada zoominLeft');
-// })
+  $('#howMany').hide();
 
-$('#howMany').hide();
-
-var dict = {
-  5: [3,2],
-  6: [4,2],
-  7: [4, 3],
-  8: [5,3],
-  9: [6,3],
-  10: [6,4]
-};
+  var dict = {
+    5: [3,2],
+    6: [4,2],
+    7: [4, 3],
+    8: [5,3],
+    9: [6,3],
+    10: [6,4]
+  };
  var num;
-$('.playerChoice').on('click', function(ev) {
-  ev.preventDefault();
-  num = parseInt($(this).find('.box').text());
-  var config = dict[num];
-  $('#goodCardCount').text(config[0]);
+  $('.playerChoice').on('click', function(ev) {
+    ev.preventDefault();
+    num = parseInt($(this).find('.box').text());
+    var config = dict[num];
+    $('#goodCardCount').text(config[0]);
+    $('#evilCardCount').text(config[1]);
+    var box_to_show = $(this).attr('class').split(' ')[0]
 
-  $('#evilCardCount').text(config[1]);
- 
-  var box_to_show = $(this).attr('class').split(' ')[0]
-
-  $('#howMany').show();
-  $('h1.anim').addClass('animated tada zoominLeft');
-  //also make the right div active
-  $(this).siblings().hide();
-  $("#restart").show();
-  $('.box' + num).show();
-  $('#activatePlayer').show();
-
-
-
- 
- 
+    $('#howMany').show();
+    $('h1.anim').addClass('animated tada zoominLeft');
   
+    $(this).siblings().hide();
+    $("#restart").show();
+    $('.box' + num).show();
+    $('#activatePlayer').show();
 });
 
 
@@ -73,8 +48,7 @@ $('.playerChoice').on('click', function(ev) {
    $('.perctitle').text('Percival');
    $('button#percival').hide();
    team.push('percival');
-   
- })
+  })
  var spots_left; 
 
 
@@ -82,6 +56,11 @@ $('.evilbutton').on('click', function(ev){
 
   var name_clicked = this.id;
   team.push(name_clicked);
+  //if  player chooses Morgana without Percival it would be useless, so we put an alert if Morgana is 
+  //chosen without Percival
+  if ((name_clicked === 'morgana') && (team.indexOf('percival') === -1)){
+  alert("For the powers of Morgana to be useful, make sure to select Percival too");
+  } 
   //I grab the count of evil cards
   var evil_card_count = $('#evilCardCount').text();
   
@@ -99,24 +78,12 @@ $('.evilbutton').on('click', function(ev){
   if (spots_left > 0) {
   add_evil_character(name_clicked, spots_left, num);
 }
-
-
 })
-var audio_files = {'':'basic.mp3',
-   'percival': 'percival.mp3'};
 
 $('#activatePlayer').on('click', function(){
-      $('#basic').show();
-      $('button').hide();
-
-      team.sort();
-      var name_audio_file = team.join();
-      alert('audio_file_name is ' + name_audio_file);
-      alert( name_audio_file === "morgana,oberon,percival");
-   
+   $('button').hide();
+      show_audio_player(team); 
     })
-
-
 });
 
 //I want to add a function that when we click on a button, it checks if there is room to add that character on deck
@@ -139,8 +106,6 @@ $('#activatePlayer').on('click', function(){
     if(spots_left === 0){
       $('.evilbuttonsdiv').hide();
     }
-
-  
   }
 
 
@@ -150,6 +115,52 @@ $('#activatePlayer').on('click', function(){
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
+
+function show_audio_player(team){
+  var audio_files = {'':'basic.mp3',
+   'percival': 'percival.mp3', 
+   'mordred': 'mordred.mp3', 
+   'morgana': 'basic.mp3', 
+   'oberon' : 'oberon.mp3',
+   'mordred,morgana': 'mordred.mp3',
+   'mordred,percival': 'Mp.mp3', 
+   'mordred,oberon': 'Mo.mp3', 
+   'mordred,morgana,percival': 'Mmp.mp3',
+   'mordred,oberon,percival': 'Mop.mp3',
+   'morgana,oberon': 'oberon.mp3',
+   'morgana,percival': 'morgPercival.mp3',
+   'morgana,oberon,percival': 'morgObePer.mp3',
+   'oberon,percival': 'obePer.mp3',
+   'mordred,morgana,oberon,percival': 'Mmop.mp3'
+ };
+   team.sort();
+
+   if (team.length === 0){
+        $('#basic').show();
+      }else if ( team.length === 1){
+          var team_name = team.join();
+          switch(team_name) {
+            case 'morgana':
+                $('#basic').show();
+                break;
+            case 'mordred':
+                $('#mordred1').show()
+                break;
+            case 'oberon':
+                $('#oberon1').show()
+                break; 
+            case 'percival':
+                $('#percival1').show()
+                break;      
+          }
+      }else{
+          var team_name = team.join();
+          var str = audio_files[team_name];
+          var id_of_target = str.substring(0, str.length - 4);
+          $('#' + id_of_target).show();
+      }
+      
+}
 
 
  
